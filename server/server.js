44b5fcf269104c2d1633313de76e9b6f1024e3b4
Rory Smith <roarsmith3@gmail.com>
2016-02-11 21:20:38 +0000

    Done stuff

--- server/server.js
+++ server/server.js
@@ -7,22 +7,39 @@ prompt.start();
 
 promptForInput();
 
-
-app.get('/', function(req, res){
-    res.sendfile('mobile.html');
-});
+app.use(express.static(__dirname + '/'));
+// app.listen(port);
 
 
+// app.get('/', function(req, res){
+//     res.sendfile('mobile.html');
+// });
+
+
+var clientCount = 0;
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    
+    socket.emit('POSITION', clientCount);
+    clientCount++;
+
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        clientCount--;
     });
-    socket.on('INFO', function(num){
+
+    socket.on('HOVER_START', function(num){
         // Send response to all
         console.log('Tone number request: ' + num);
-        io.emit('SONG', requestToneNumber(num));
+        io.emit('SONG', {'toneNumber': num, 'state': 1});
+    });
+
+    socket.on('HOVER_END', function(num){
+        // Send response to all
+        console.log('Tone number request: ' + num);
+        io.emit('SONG', {'toneNumber': num, 'state': 0});
     });
 });
 
@@ -30,17 +47,13 @@ http.listen(3000, function(){
     console.log('listening on *:3000');
 });
 
-function requestToneNumber(number){
-    var response = {
-        toneNumber: number
-    };
-    return JSON.stringify(response);
-}
+
 function sendBaseLayerRequest() {
     // Tell clients to take base layer
     console.log('Telling clients to take base layer');
     io.emit('BASE', 1);
 }
+
 function promptForInput(){
     prompt.get('val', function (err, result) {
         sendBaseLayerRequest();
